9084b7381ace43cffd08bd5461a093c5fb5d7364
Toke Eskildsen <te@ekot.dk>
2013-09-26 22:12:30 +0200

    Some random hacking

--- generate_samples.sh
+++ generate_samples.sh
@@ -13,6 +13,10 @@ TEXT="Noster laboramus no ius, graece doctus no quo. Eam ea dicta clita, probo o
 MARGIN=20
 MIN_WIDTH=540
 MIN_HEIGHT=1000
+HEADER_HEIGHT=200
+COL_MIN_WIDTH=500
+
+COUNTER=1
 
 # Input: filename imagetype width height
 function generate() {
@@ -31,12 +35,34 @@ function generate() {
         exit 2
     fi
 
-    local COLUMNS=`echo "\($WIDTH - 2 * $MARGIN\) % 
+    local COLUMNS=`echo "($WIDTH - 2 * $MARGIN) / $COL_MIN_WIDTH" | bc`
+    local COLUMN_WIDTH=`echo "($WIDTH - 2 * $MARGIN) / $COLUMNS" | bc`
+    local COLUMN_HEIGHT=`echo "$HEIGHT - 2 * $MARGIN - $HEADER_HEIGHT" | bc`
 
-    echo "Generating $TYPE and ALTO XML for '$BASE' of ${WIDTH}x${HEIGHT} pixels"
-
-
+    echo "Generating #${COUNTER} $TYPE and ALTO XML for '$BASE' of ${WIDTH}x${HEIGHT} pixels with $COLUMNS columns of ${COLUMN_WIDTH}x${COLUMN_HEIGHT} pixels"
     
+    C="-size ${WIDTH}x${HEIGHT} canvas:'#f0f0f0'"
+    # TODO: Add light grey gaussian noise and darker speckles to simulate scanned paper
+#    C="$C +noise Gaussian"
+
+    # Generate header
+    HEADER="$BASE $COUNTER"
+    local HEADER_Y=`echo "$HEIGHT / 2 - $MARGIN - $HEADER_HEIGHT" | bc`
+    C="$C -gravity center -font Helvetica -pointsize 150 -fill black -draw \"text 0,-$HEADER_Y '$HEADER'\""
+
+    # Add columns
+    
+    
+    C="$C \"${BASE}.png\""
+    echo "convert $C"
+    eval "convert $C"
+    
+    COUNTER=`echo "$COUNTER + 1" | bc`
 }
+
+if [ "." == ".$1" -o "." == ".$2" -o "." == ".$3" ]; then
+    echo "Usage: ./generate_samples folder width height"
+    exit 2
+fi
 
 generate $1 png $2 $3
