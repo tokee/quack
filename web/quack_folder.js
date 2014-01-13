2cb1760527441954fe141b11a2aebaa2bce5e278
Toke Eskildsen <te@ekot.dk>
2014-01-13 21:29:52 +0100

    Bugfix: The overlays in folder did not work after sorttable was introduced

--- web/quack_folder.js
+++ web/quack_folder.js
@@ -10,7 +10,7 @@ function toggleBlownThumbs() {
         content[i].style.display = state;
     }
 }
-function toggleHistograms() {    
+function toggleHistograms() {  
     if (document.getElementById('toggle_histograms').checked) {
         thumb_state = 'none';
         hist_state = 'block';
@@ -29,7 +29,19 @@ function toggleHistograms() {
     }
 }
 
-function setupThumbOverlays() {
+function thclick() {
+    // Consider a timeout if there is a race condition with the table sort
+    // TODO: Order the image divs like the table links are ordered
+}
+
+var oldOnload = window.onload;
+function initialSetup() {
+    if (oldOnload != null) {
+        console.log('Calling previous onLoad');
+        oldOnload();
+    }
+    // Thumb overlays
+    console.log('Adding thumb overlays');
     var content = document.getElementsByClassName('thumblink');
     for (var i = 0 ; i < content.length ; i++) {
         var href = content[i].href;
@@ -42,5 +54,16 @@ function setupThumbOverlays() {
         span.style.background = "url(" + base + ".black.thumb.png) 100% 100%, url(" + base + ".white.thumb.png) 100% 100%";
         document.title = span.style.background;
     }
+
+    console.log('Attaching hooks for table sort');
+    var tables = document.getElementsByClassName('sortable');
+    for (var i = 0 ; i < tables.length ; i++) {
+        var stable = tables[i];
+        var theads = stable.getElementsByTagName('th');
+        for (var h = 0 ; h < theads.length ; h++) {
+            thead = theads[h];
+            thead.onclick = thclick;
+        }
+    }
 }
-window.onload=setupThumbOverlays;
+window.onload = initialSetup;
